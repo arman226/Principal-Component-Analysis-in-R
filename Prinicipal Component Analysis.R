74885850be90b788d7f61bc2b49290f5652b9723
armando bibay jr <apple@apples-MacBook-Pro.local>
2022-11-25 18:10:53 +0800

    quick way of evaluation

--- Prinicipal Component Analysis.R
+++ Prinicipal Component Analysis.R
@@ -1,7 +1,6 @@
 # install pre-requisite packages
-install.packages("arules")
-install.packages("stats")
-install.packages("dplyr")
+install.packages("stats") #has functionalities that will help us execute Principal Components Analysis
+install.packages("dplyr") #helps us with data manipulation
 
 # load the required libraries 
 
@@ -9,14 +8,15 @@ library(stats)
 library(dplyr)
 
 
-# using Iris Dataset
+# using Iris Dataset let's inspect the data using the View function
 View(iris)
-
+# There are 5 columns in the Iris data set namely Sepal.length Sepal Width Petal Length and PEtal Width, and Species, but since PCA is a branch of
+# Unsupervised Learning, we need unlabeled data, thus we need to ignore the Species column
 # Unsupervised Learning - Hence converting iris data to unlabeled data set
-mydata = select(iris, c(1,2,3,4)) # Select from the Iris Data Set and then choose the first 4 columns
-mydata
+mydata = select(iris, c(1,2,3,4)) # Select from the Iris Data Set and then choose the first 4 columns (Ignore the Fifth Column)
+View(mydata)
 
-# Check the correlation among the variables or predictors
+ # Check the correlation among the variables or predictors
 # in other words, we have to check if they are LINEARLY DEPENDENT
 # Correlation Matrix can help check whether or not the variables are highly correlated 
 # If the average correlation is above 0.3 or below -0.3, then the variables are highly correlated with one another AND
@@ -35,9 +35,17 @@ View(PCA)
 # 2. Check if the PCs are independent
 
 
-#PC Loadings
-PCA$loadings
+#PC Loadings  
+PCA$loadings #the loading matrix will help us evaluate how the principal components capture the ESSENCE of the original variables
+
+
+#Take note that the number of the components built will ALWAYS be EQUAL to the number of VARIABLES
 
 # Princiapl Components $
+# ths scores matrix will help us evaluate if the PCs are independent
+# if the PCs are independent, then the correlation must be 0 or must be nearly 0
 PC <- PCA$scores
+View(PC)
+cor(PC)
+
 
